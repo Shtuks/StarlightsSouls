fcb9b04de74b7cd4cb9becf0c6f21b0b43760aba
Rowen Berry <rcberry2010@gmail.com>
2025-04-29 07:59:00 -0500

    Update ShtunConfig.cs
    Added the gunrightsmod to ShtunConfig.
    If there is a way to make multiple changes at once and this is bothering you, the way I am doing stuff one by one, feel free to tell me.

--- ShtunConfig.cs
+++ ShtunConfig.cs
@@ -82,5 +82,10 @@ namespace ssm
         [BackgroundColor(60, 200, 60, 192)]
         [DefaultValue(true)]
         public bool Beekeeper { get; set; }
+
+        [ReloadRequired]
+        [BackgroundColor(60, 200, 60, 192)]
+        [DefaultValue(true)]
+        public bool gunrightsmod { get; set; }
     }
 }
