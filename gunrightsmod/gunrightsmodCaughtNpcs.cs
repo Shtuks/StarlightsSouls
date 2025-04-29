3b710d88e9d7efd83e735a10b8764f85812cadc8
Rowen Berry <rcberry2010@gmail.com>
2025-04-29 09:07:25 -0500

    Create gunrightsmodCaughtNpcs.cs
    Added a CaughtNPCs file to gunrightsmod. 
    Note: Ask Autism Manifesto what they want the tooltip to be?

---
+++ gunrightsmod/gunrightsmodCaughtNpcs.cs
@@ -0,0 +1,17 @@
+using ssm.Items;
+using Terraria.ModLoader;
+using ssm.Core;
+using gunrightsmod.Content.NPCs.Politician;
+
+namespace ssm.gunrightsmod
+{
+    [ExtendsFromMod(ModCompatibility.gunrightsmod.Name)]
+    [JITWhenModsEnabled(ModCompatibility.gunrightsmod.Name)]
+    internal class gunrightsmodCaughtNpcs : ModSystem
+    {
+        public static void gunrightsmodRegisterItems()
+        {
+            CaughtNPCItem.Add("Politician", ModContent.NPCType<Politician>(), "''");
+        }
+    }
+}
