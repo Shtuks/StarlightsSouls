3a9aa5462de1e273e02fdcb5b5f6b3a4e49b4d3b
Rowen Berry <rcberry2010@gmail.com>
2025-04-30 10:10:03 -0500

    Create RevolutionSoul.cs

---
+++ gunrightsmod/Souls/RevolutionSoul.cs
@@ -0,0 +1,67 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using gunrightsmod;
+using FargowiltasSouls.Content.Items.Accessories.Souls;
+using Fargowiltas.Items.Tiles;
+using ssm.gunrightsmod.Enchantments;
+using ssm.gunrightsmod.Forces;
+using ssm.Systems;
+using ssm.Core;
+using FargowiltasSouls.Content.Items.Materials;
+using gunrightsmod.Content.Items.Materials;
+
+namespace ssm.gunrightsmod.Souls
+{
+	[ExtendsFromMod(ModCompatibility.SacredTools.Name)]
+	[JITWhenModsEnabled(ModCompatibility.SacredTools.Name)]
+	public class SoASoul : BaseSoul
+	{
+		public override bool IsLoadingEnabled(Mod mod)
+		{
+			return ShtunConfig.Instance.gunrightsmod;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 20;
+			Item.height = 20;
+			Item.defense = 25;
+			Item.accessory = true;
+			ItemID.Sets.ItemNoGravity[Item.type] = true;
+			Item.rare = 11;
+			Item.value = 1000000;
+		}
+
+	/*	
+ 
+ Once everything else is done, I will remove this
+ 
+ public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			ModdedPlayer modPlayer = player.GetModPlayer<ModdedPlayer>();
+
+			ModContent.Find<ModItem>(((ModType)this).Mod.Name, "Force1").UpdateAccessory(player, hideVisual);
+			ModContent.Find<ModItem>(((ModType)this).Mod.Name, "Force2").UpdateAccessory(player, hideVisual);
+			ModContent.Find<ModItem>(((ModType)this).Mod.Name, "Force3").UpdateAccessory(player, hideVisual);
+			ModContent.Find<ModItem>(((ModType)this).Mod.Name, "Force4").UpdateAccessory(player, hideVisual);
+		}
+
+	*/
+
+/*	public override void AddRecipes()
+		{
+			Recipe recipe = this.CreateRecipe();
+			recipe.AddIngredient<Force1>();
+			recipe.AddIngredient<Force2>();
+			recipe.AddIngredient<Force3>();
+			recipe.AddIngredient<Force4>();
+			recipe.AddIngredient<Item1>(__________);
+			recipe.AddIngredient<Item2>(_____);
+			recipe.AddTile<______WhereYouCraft_____>();
+			recipe.Register();
+		}
+	}
+}
+*/
