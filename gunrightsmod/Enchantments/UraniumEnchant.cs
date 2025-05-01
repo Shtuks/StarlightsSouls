d78a1d5caf7fdb5262e6f48e00d37856589b131d
Rowen Berry <rcberry2010@gmail.com>
2025-05-01 12:06:59 -0500

    Create UraniumEnchant.cs
    Created the Uranium Enchantment

---
+++ gunrightsmod/Enchantments/UraniumEnchant.cs
@@ -0,0 +1,99 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using gunrightsmod.Content.Items;
+using ssm.Core;
+using Terraria.DataStructures;
+
+namespace ssm.gunrightmod.Enchantments
+{
+    [ExtendsFromMod(ModCompatibility.gunrightsmod.Name)]
+    [JITWhenModsEnabled(ModCompatibility.gunrightsmod.Name)]
+    public class UraniumEnchant : BaseEnchant
+    {
+        public override bool IsLoadingEnabled(Mod mod)
+        {
+            return ShtunConfig.Instance.gunrightsmod;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.accessory = true;
+            ItemID.Sets.ItemNoGravity[Item.type] = true;
+            Item.rare = 6;
+            Item.value = 200000;
+        }
+
+        public override Color nameColor => new(255, 128, 0);
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<UraniumAuraEffect>().auraActive = true;
+        }
+
+        public class UraniumAuraEffect : ModPlayer
+        {
+            public bool auraActive;
+
+            public override void ResetEffects()
+            {
+                auraActive = false;
+            }
+
+            public override void PostUpdate()
+            {
+                if (!auraActive)
+                    return;
+
+                int radius = 160;
+                int damage = (int)(50 * Player.GetDamage(DamageClass.Generic).Additive);
+
+                foreach (NPC npc in Main.npc)
+                {
+                    if (npc.active && !npc.friendly && !npc.dontTakeDamage && npc.Distance(Player.Center) < radius)
+                    {
+                        npc.StrikeNPCNoInteraction(damage, 0f, 0, false, false, false);
+                    }
+                }
+            }
+
+            public override void PostDraw()
+            {
+                if (!auraActive)
+                    return;
+
+                Texture2D texture = ModContent.Request<Texture2D>("gunrightsmod/Textures/UraniumAuraCircle").Value;
+                Vector2 position = Player.Center - Main.screenPosition;
+                Vector2 origin = new Vector2(texture.Width / 2f, texture.Height / 2f);
+
+                Main.spriteBatch.Draw(
+                    texture,
+                    position,
+                    null,
+                    Color.Green * 0.3f,
+                    0f,
+                    origin,
+                    1f,
+                    SpriteEffects.None,
+                    0f
+                );
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<UraniumHelmet>());
+            recipe.AddIngredient(ModContent.ItemType<UraniumChestplate>());
+            recipe.AddIngredient(ModContent.ItemType<UraniumLeggings>());
+            recipe.AddIngredient(ModContent.ItemType<UraniumSword>());
+            recipe.AddIngredient(ModContent.ItemType<PlasmoidWand>());
+            recipe.AddIngredient(ModContent.ItemType<ParticleGun>());
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.Register();
+        }
+    }
+
+}
